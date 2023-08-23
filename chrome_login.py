8dbd4a8cb4cc9fa1f191e1472cd55c15ee7d04b8
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2023-08-23 12:11:36 +0530

    added selenium service

--- chrome_login.py
+++ chrome_login.py
@@ -1,11 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 def login(url,user,pwd,dbName) : 
-    driver = webdriver.Chrome("../chromedriver.exe")
+    service = Service(executable_path="../chromedriver.exe")
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=service, options=options)
     driver.get(f"{url}/rsunify/")
     for [k,v] in [["userName",user],["databaseName",dbName],["password",pwd]] : 
         WebDriverWait(driver, 10).until(
